aefb757c6ae0c4b13bb4deb8b99050673e228560
Adrian <73155005+adrian-javid@users.noreply.github.com>
2024-01-28 02:35:28 -0600

    use simple SDL functions

--- source/main.cpp
+++ source/main.cpp
@@ -1,8 +1,15 @@
 
 #include <iostream>
-#include "SDL2/SDL.h"
 
-int main(int argc, char **argv) {
+#ifdef _WIN64
+#include "SDL2/SDL.h"
+#else
+static_assert(false)
+#endif
+
+int main(int argc, char* argv[]) {
+    SDL_Init(0);
     std::cout << "Begin.\n";
     return EXIT_SUCCESS;
+    SDL_Quit();
 }
