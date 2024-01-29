206c5e7e39d2df43986f7fe85a420dda48dbc384
Adrian <73155005+adrian-javid@users.noreply.github.com>
2024-01-28 19:04:42 -0600

    move code to source file

---
+++ source/SquareGrid.cpp
@@ -0,0 +1,33 @@
+#include "SquareGrid.hpp"
+
+using csm4880::SquareGrid;
+
+SquareGrid::SquareGrid(size_t const rowCount, size_t const columnCount):
+    flatData(rowCount * columnCount), rowCount{rowCount}, columnCount{columnCount}
+{}
+
+size_t SquareGrid::getRowCount() const { return rowCount; }
+size_t SquareGrid::getColumnCount() const { return columnCount; }
+auto SquareGrid::getFlatData() const -> Table const & { return flatData; }
+
+auto SquareGrid::at(size_t const row, size_t const column) -> Tile & { return flatData.at(row * columnCount + column); }
+auto SquareGrid::at(size_t const row, size_t const column) const -> Tile const & { return flatData.at(row * columnCount + column); }
+
+std::string SquareGrid::toString(char const wallSymbol, char const emptySymbol) const {
+    std::stringstream buffer;
+
+    for (size_t row{0}; row < rowCount; ++row) {
+    for (size_t column{0}; column < columnCount; ++column) {
+        Tile const tile = at(row, column);
+        char const symbol = (tile ? wallSymbol : emptySymbol);
+        buffer << ' ' << symbol;
+    }
+    buffer << ' ' << '\n';
+    }
+
+    return buffer.str();
+}
+
+std::ostream& csm4880::operator<<(std::ostream &outputStream, SquareGrid const &squareGrid) {
+    outputStream << squareGrid.toString(); return outputStream;
+}
--- source/SquareGrid.hpp
+++ source/SquareGrid.hpp
@@ -27,35 +27,17 @@ class csm4880::SquareGrid {
 
   public:
 
-    explicit SquareGrid(size_t const rowCount, size_t const columnCount):
-      flatData(rowCount * columnCount), rowCount{rowCount}, columnCount{columnCount}
-    {}
+    explicit SquareGrid(size_t const rowCount, size_t const columnCount);
+    size_t getRowCount() const;
+    size_t getColumnCount() const;
+    Table const &getFlatData() const;
 
-    size_t getRowCount() const { return rowCount; }
-    size_t getColumnCount() const { return columnCount; }
-    Table const &getFlatData() const { return flatData; }
+    Tile &at(size_t const row, size_t const column);
+    Tile const &at(size_t const row, size_t const column) const;
 
-    Tile &at(size_t const row, size_t const column) { return flatData.at(row * columnCount + column); }
-    Tile const &at(size_t const row, size_t const column) const { return flatData.at(row * columnCount + column); }
+    std::string toString(char const wallSymbol='#', char const emptySymbol='.') const;
 
-    std::string toString(char const wallSymbol='#', char const emptySymbol='.') const {
-      std::stringstream buffer;
-
-      for (size_t row{0}; row < rowCount; ++row) {
-        for (size_t column{0}; column < columnCount; ++column) {
-          Tile const tile = at(row, column);
-          char const symbol = (tile ? wallSymbol : emptySymbol);
-          buffer << ' ' << symbol;
-        }
-        buffer << ' ' << '\n';
-      }
-
-      return buffer.str();
-    }
-
-    friend std::ostream& operator<<(std::ostream &outputStream, SquareGrid const &squareGrid) {
-      outputStream << squareGrid.toString(); return outputStream;
-    }
+    friend std::ostream& operator<<(std::ostream &outputStream, SquareGrid const &squareGrid);
 
 };
 
