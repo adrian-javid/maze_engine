cb4a86f8dc664e97063d4a4a5a0ba6cbf7d971c1
Adrian <73155005+adrian-javid@users.noreply.github.com>
2024-03-25 23:42:54 -0500

    use `CommonInt` with `Grid`

--- source/main/Grid.cpp
+++ source/main/Grid.cpp
@@ -3,5 +3,5 @@
 
 using namespace Project;
 
-bool Grid::isWall(Index const row, Index const column) const { return at(row, column) & Grid::wallFlag; }
-void Grid::putWall(Index const row, Index const column) { at(row, column) |= Grid::wallFlag; }
+bool Grid::isWall(CommonInt const row, CommonInt const column) const { return at(row, column) & Grid::wallFlag; }
+void Grid::putWall(CommonInt const row, CommonInt const column) { at(row, column) |= Grid::wallFlag; }
--- source/main/Grid.hpp
+++ source/main/Grid.hpp
@@ -1,7 +1,8 @@
 #ifndef Grid_hpp
 #define Grid_hpp
 
-#include <cstdint>
+#include "CommonInt.hpp"
+
 #include <functional>
 
 namespace Project { class Grid; }
@@ -11,20 +12,19 @@ class Project::Grid {
   public:
 
     using Tile = uint_fast8_t;
-    using Index = int_fast32_t;
 
     constexpr static Tile emptyTile = 0;
     constexpr static Tile wallFlag = 1 << 0;
 
-    virtual Tile &at(Index const row, Index const column) = 0;
-    virtual Tile const &at(Index const row, Index const column) const = 0;
+    virtual Tile &at(CommonInt const row, CommonInt const column) = 0;
+    virtual Tile const &at(CommonInt const row, CommonInt const column) const = 0;
 
-    bool isWall(Index const row, Index const column) const;
-    void putWall(Index const row, Index const column);
+    bool isWall(CommonInt const row, CommonInt const column) const;
+    void putWall(CommonInt const row, CommonInt const column);
 
     // virtual void forNeighbor(
-    //   Index const row, Index const column,
-    //   std::function<void(Index const, Index const)> const 
+    //   CommonInt const row, CommonInt const column,
+    //   std::function<void(CommonInt const, CommonInt const)> const 
     // ) const;
 
 };
