1fa8ba9df467066913bf49f998d21a45c092f66d
Adrian <73155005+adrian-javid@users.noreply.github.com>
2024-03-27 02:13:15 -0500

    add max constant

--- source/main/CommonInt.hpp
+++ source/main/CommonInt.hpp
@@ -4,8 +4,12 @@
 #include <cstdint>
 
 namespace Project {
+
     /// @brief Fastest signed integral type that is at least 32 bits.
     using CommonInt = std::int_fast32_t;
+
+    constexpr std::size_t commonIntMaxLimit = static_cast<std::size_t>(std::numeric_limits<CommonInt>::max());
+
 }
 
 #endif
