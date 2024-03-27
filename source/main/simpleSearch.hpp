04bf343203944911f09ca995f14ff71e8fb64616
Adrian <73155005+adrian-javid@users.noreply.github.com>
2024-03-27 00:39:04 -0500

    use `Grid`

--- source/main/simpleSearch.hpp
+++ source/main/simpleSearch.hpp
@@ -2,34 +2,24 @@
 #ifndef simpleSearch_hpp
 #define simpleSearch_hpp
 
-#include <optional>
-#include <vector>
-#include <array>
 #include <type_traits>
 #include <stack>
 #include "Cast.hpp"
 
-#include "Vector2.hpp"
-
 namespace Project {
     template <typename Storage_T>
     std::optional<std::vector<Vector2>> simpleSearch(
-        SquareGrid const &, Vector2 const &, Vector2 const &
+        Grid const &, Vector2 const &, Vector2 const &
     );
 }
 
 template <typename Storage_T>
 std::optional<std::vector<Project::Vector2>> Project::simpleSearch(
-    SquareGrid const &grid,
+    Grid const &grid,
     Vector2 const &start,
     Vector2 const &end
 ) {
     using namespace Project;
-
-    static const std::array<Vector2, 4> cardinalDirectionList = {Vector2::north, Vector2::south, Vector2::east, Vector2::west};
-
-    int const rowCount = Cast::toInt(grid.getRowCount());
-    int const columnCount = Cast::toInt(grid.getColumnCount());
 
     Storage_T storage;
     Vector2::HashMap<Vector2> upTree;
