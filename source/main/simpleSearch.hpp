8b3601a61d90ad93118696b99b55d0a1ca362293
Adrian <73155005+adrian-javid@users.noreply.github.com>
2024-03-26 22:14:20 -0500

    conditional compilation for `std::stack`

--- source/main/simpleSearch.hpp
+++ source/main/simpleSearch.hpp
@@ -5,6 +5,8 @@
 #include <optional>
 #include <vector>
 #include <array>
+#include <type_traits>
+#include <stack>
 #include "cast.hpp"
 
 #include "Vector2.hpp"
@@ -24,7 +26,7 @@ std::optional<std::vector<Project::Vector2>> Project::simpleSearch(
 ) {
     using namespace Project;
 
-    static std::array<Vector2, 4> const cardinalDirectionList = {Vector2::north, Vector2::south, Vector2::east, Vector2::west};
+    static const std::array<Vector2, 4> cardinalDirectionList = {Vector2::north, Vector2::south, Vector2::east, Vector2::west};
 
     int const rowCount = Cast::toInt(grid.getRowCount());
     int const columnCount = Cast::toInt(grid.getColumnCount());
@@ -36,7 +38,12 @@ std::optional<std::vector<Project::Vector2>> Project::simpleSearch(
     map.insert({start, start});
 
     while (not storage.empty()) {
-        Vector2 const vector = storage.front();
+        Vector2 const vector = [&storage]() constexpr -> Vector2 const & {
+            if constexpr (std::is_same_v<Storage_T, std::stack<Vector2>>)
+                return storage.top();
+            else
+                return storage.front();
+        }();
         storage.pop();
 
         if (vector == end) {
