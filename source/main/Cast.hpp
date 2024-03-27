e60b78118d57ba3bacb6504b82f3df085686c5a6
Adrian <73155005+adrian-javid@users.noreply.github.com>
2024-03-27 02:15:44 -0500

    return `std::nullopt` if `std::size_t` can't fit in `CommonInt`

--- source/main/Cast.hpp
+++ source/main/Cast.hpp
@@ -4,14 +4,15 @@
 #include "CommonInt.hpp"
 #include <cstddef>
 #include <limits>
+#include <optional>
 
 namespace Project::Cast {
 
-    constexpr auto toInt(std::size_t const value) -> CommonInt {
-        if (value <= static_cast<std::size_t>(std::numeric_limits<CommonInt>::max()))
+    constexpr auto toInt(std::size_t const value) -> std::optional<CommonInt> {
+        if (value <= commonIntMaxLimit)
             return static_cast<CommonInt>(value);
         else
-            return std::numeric_limits<CommonInt>::max();
+            return std::nullopt;
     }
 
 }
