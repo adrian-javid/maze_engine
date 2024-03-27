8e615cf38339c59092871277d968ad3c041d50d1
Adrian <73155005+adrian-javid@users.noreply.github.com>
2024-03-26 23:00:11 -0500

    proper `constexpr`

--- source/main/Cast.hpp
+++ source/main/Cast.hpp
@@ -2,9 +2,18 @@
 #define Cast_hpp
 
 #include "CommonInt.hpp"
+#include <cstddef>
+#include <limits>
 
 namespace Project::Cast {
-    constexpr CommonInt toInt(size_t const value);
+
+    constexpr auto toInt(std::size_t const value) -> CommonInt {
+        if (value <= static_cast<std::size_t>(std::numeric_limits<CommonInt>::max()))
+            return static_cast<CommonInt>(value);
+        else
+            return std::numeric_limits<CommonInt>::max();
+    }
+
 }
 
 #endif
