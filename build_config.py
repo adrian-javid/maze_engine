eb440d8e050bf40da39fd7b5d03fcfc1e8d75a68
Adrian <73155005+adrian-javid@users.noreply.github.com>
2024-04-09 13:25:53 -0500

    add warning flag for return local address

--- build_config.py
+++ build_config.py
@@ -57,6 +57,8 @@ GCC_WARNING = make(CXXFLAGS=[
     '-Wold-style-cast', '-Wcast-qual', '-Wcast-function-type',
     '-Wcast-align', '-Wunused-macros', '-Wzero-as-null-pointer-constant',
 
+    '-Wreturn-local-addr',
+
     '-Wundef', # undefined identifier in `#if` directive
     '-Wnon-virtual-dtor', # virtual functions but non-virtual destructor
     '-Wdouble-promotion', # float implicitly promoted to double
