3db3c94e58da94952846dab201a7db74c0548a26
Adrian <73155005+adrian-javid@users.noreply.github.com>
2024-04-10 14:03:35 -0500

    Hexagonal grid class.

---
+++ source/main/HexagonGrid.hpp
@@ -0,0 +1,12 @@
+#ifndef HexagonGrid_hpp
+#define HexagonGrid_hpp
+
+#include "Grid.hpp"
+
+namespace Project { class HexagonGrid; }
+
+class Project::HexagonGrid : public Project::Grid {
+
+};
+
+#endif
