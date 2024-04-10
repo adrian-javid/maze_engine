805e9c2810ca0a73a751d4586dcff4c78ae5ef0b
Adrian <73155005+adrian-javid@users.noreply.github.com>
2024-04-10 16:40:44 -0500

    calculate mirror center

--- source/main/HexagonGrid.cpp
+++ source/main/HexagonGrid.cpp
@@ -1,3 +1,53 @@
 #include "HexagonGrid.hpp"
 
-//// ...
+using namespace Project;
+
+static Vector2 calculateMirrorCenter(int const index, int const radius) {
+    constexpr Vector2 center(0, 0);
+    Vector2 const startPosition{2 * radius + 1, -radius};
+    Vector2 const difference = startPosition - center;
+    Vector2 const rotatedDifference = difference.hexagonalRotate(index);
+    Vector2 const rotatedPosition = rotatedDifference + center;
+    return rotatedPosition;
+}
+
+HexagonGrid::HexagonGrid(int const setRadius):
+    table(),
+    mirrorCenterTable{
+        calculateMirrorCenter(0, setRadius),
+        calculateMirrorCenter(1, setRadius),
+        calculateMirrorCenter(2, setRadius),
+        calculateMirrorCenter(3, setRadius),
+        calculateMirrorCenter(4, setRadius),
+        calculateMirrorCenter(5, setRadius),
+    },
+    radius{radius}
+{
+    assert(radius >= 0);
+
+    constexpr Vector2 center(0, 0);
+
+    for (int q = -radius; q <= radius; ++q) {
+        int r1 = std::max<int>(-radius, -q - radius);
+        int r2 = std::min<int>( radius, -q + radius);
+
+        for (int r = r1; r <= r2; ++r) {
+            Vector2 const key(center + Vector2(q, r));
+            table.insert({key, Project::Grid::emptyTile});
+        }
+    }
+}
+
+auto HexagonGrid::at(Vector2 const &key) -> Tile & { return table.at(key); }
+
+auto HexagonGrid::at(Vector2 const &key) const -> Tile const & { return table.at(key); }
+
+auto HexagonGrid::at(int const axis1, int const axis2) -> Tile & { return table.at({axis1, axis2}); }
+
+auto HexagonGrid::at(int const axis1, int const axis2) const -> Tile const & {
+    return table.at({axis1, axis2});
+}
+
+void HexagonGrid::forNeighbor(Vector2 const &, std::function<void(Vector2 const &)>) const {
+
+}
--- source/main/HexagonGrid.hpp
+++ source/main/HexagonGrid.hpp
@@ -3,10 +3,31 @@
 
 #include "Grid.hpp"
 
+#include <array>
+
 namespace Project { class HexagonGrid; }
 
 class Project::HexagonGrid : public Project::Grid {
 
+  private:
+
+    Vector2::HashMap<Grid::Tile> table;
+    std::array<Vector2, /* A hexagon has six sides. */ 6> mirrorCenterTable;
+    int radius;
+
+  public:
+
+    explicit HexagonGrid(int const setRadius=0);
+
+    constexpr int Radius() const { return radius; }
+
+    Tile &at(int const axis1, int const axis2) override;
+    Tile const &at(int const axis1, int const axis2) const override;
+
+    virtual Tile &at(Vector2 const &key) override;
+    virtual Tile const &at(Vector2 const &key) const override;
+
+    void forNeighbor(Vector2 const &, std::function<void(Vector2 const &)>) const override;
 };
 
 #endif
