f068dd00cdf4cb15164a3ff6905fdd88680e1aa8
Adrian <73155005+adrian-javid@users.noreply.github.com>
2024-04-09 19:42:59 -0500

    get color triplet

--- source/color.hpp
+++ source/color.hpp
@@ -2,9 +2,11 @@
 #define color_hpp
 
 #include "simpleDirectmediaLayer.hpp"
+#include "Util.hpp"
 
 #include <tuple>
 #include <string>
+#include <cassert>
 
 namespace Project::Media {
     using ColorTriplet = std::tuple<SDL_Color, SDL_Color, SDL_Color>;
@@ -17,7 +19,9 @@ namespace Project::Media {
 }
 
 struct Project::Media::HslaColor {
+
     double hue, saturation, luminance, alpha;
+
     constexpr HslaColor(
         double const hueValue,
         double const saturationValue=1.0,
@@ -29,6 +33,27 @@ struct Project::Media::HslaColor {
         luminance{luminanceValue},
         alpha{alphaValue}
     {}
+
+    inline ColorTriplet getColorTriplet(double const percentage, double const colorLength) const {
+        double const startValue{this->hue};
+        double const endValue{startValue + colorLength};
+
+        double const value{Util::linearInterpolation(percentage, startValue, startValue + 2 * colorLength)};
+
+        constexpr double fullCycle{360.0};
+
+        double const hueValue =
+            value < endValue ?
+                Util::wrapValue(value, fullCycle) :
+                Util::wrapValue(endValue - (value - endValue), fullCycle);
+
+        return {
+            this->toRgbaColor(),
+            this->toRgbaColor(-20.0),
+            this->toRgbaColor(-40.0)
+        };
+    }
+
     SDL_Color toRgbaColor(double hueSupplement=0.0) const;
     static double wrapHue(double hue, double const bound=360.0);
     void addHue(double const hueSupplement);
