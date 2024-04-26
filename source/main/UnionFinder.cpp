535eaceab8f27cccabc4871a91d8e8c45fb3897d
Adrian <73155005+adrian-javid@users.noreply.github.com>
2024-04-25 22:48:42 -0500

    union find

--- source/main/UnionFinder.cpp
+++ source/main/UnionFinder.cpp
@@ -1,5 +1,11 @@
 #include "UnionFinder.hpp"
 
+Project::UnionFinder::UnionFinder(std::size_t const count): forest(count, -1) {}
+
+int Project::UnionFinder::addElements(std::size_t const count) {
+    forest.insert(forest.end(), count, -1);
+}
+
 int Project::UnionFinder::find(int const element) {
     // base case
     if (forest.at(element) < 0) return element;
@@ -11,4 +17,23 @@ int Project::UnionFinder::find(int const element) {
     forest.at(element) = root;
 
     return root;
+}
+
+void Project::UnionFinder::unionThem(int const element1, int const element2) {
+  int const root1 = find(element1);
+  int const root2 = find(element2);
+  int const negativeSize = forest.at(root1) + forest.at(root2);
+
+  if (forest.at(root1) < forest.at(root2)) {
+    forest.at(root2) = root1;
+    forest.at(root1) = negativeSize;
+  } else {
+    forest.at(root1) = root2;
+    forest.at(root2) = negativeSize;
+  }
+}
+
+int Project::UnionFinder::size(int const element) {
+    int const root = find(element);
+    return -1 * forest.at(root);
 }
--- source/main/UnionFinder.hpp
+++ source/main/UnionFinder.hpp
@@ -1,6 +1,26 @@
 #ifndef UnionFinder_hpp
 #define UnionFinder_hpp
 
+#include <vector>
+
 namespace Project { class UnionFinder; }
+
+class Project::UnionFinder {
+  private:
+    std::vector<int> forest;
+
+  public:
+
+    UnionFinder(std::size_t const count=0u);
+
+    int addElements(std::size_t const count);
+
+    void unionThem(int const element1, int const element2);
+
+    int find(int const element);
+
+    int size(int const element);
+
+};
 
 #endif
