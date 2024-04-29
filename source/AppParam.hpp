b34e0904c7b3e39af000260cad82ed4b910c8392
Adrian <73155005+adrian-javid@users.noreply.github.com>
2024-04-28 19:10:09 -0500

    cast arg

--- source/AppParam.hpp
+++ source/AppParam.hpp
@@ -13,6 +13,25 @@ namespace Project {
         std::optional<Acceptable> const acceptable;
         static std::unordered_map<std::string, AppParam> const &parseArgv(int const argc, char const *const *const argv);
 
+        template<typename T>
+        static T castArg(std::string const &arg) {
+            constexpr auto getTypeName = []() constexpr -> std::string {
+                if constexpr (std::is_same_v<T, int>)
+                    return "int";
+                else if constexpr (std::is_same_v<T, bool>)
+                    return "bool";
+                else
+                    static_assert(false, "Unsupported type.");
+            }
+
+            T value{};
+            if ((std::istringstream(arg) >> value).fail()) Util::errOutLn(
+                "Unable to convert `" + arg + "` to `" + getTypeName + "` value."
+            );
+
+            return value;
+        }
+
         private:
             static std::unordered_map<std::string, AppParam> config;
     };
