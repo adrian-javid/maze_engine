3499c6c68e6b08789e2dd45ad1aa4982799a0b44
Adrian <73155005+adrian-javid@users.noreply.github.com>
2024-04-30 04:17:35 -0500

    implement "greater than"

--- source/main/aStarSearch.cpp
+++ source/main/aStarSearch.cpp
@@ -10,12 +10,15 @@ auto Project::aStarSearch(
 ) -> Vector2::HashMap<Vector2> {
     struct MetaVertex {
         Vector2 key;
+        // Smaller priority is better.
         int priority;
-        inline bool operator>(MetaVertex const &vertex) const;
+        inline bool operator>(MetaVertex const &vertex) const {
+            return priority > vertex.priority;
+        }
     };
 
     std::priority_queue<MetaVertex, std::vector<MetaVertex>, std::greater<MetaVertex>> frontier;
-    frontier.emplace(start, 0);
+    frontier.push({start, 0});
 
     Vector2::HashMap<Vector2> upTree{{start, start}};
     Vector2::HashMap<int> costMap{{start, 0}};
@@ -27,14 +30,13 @@ auto Project::aStarSearch(
         if (processKey != nullptr and processKey(vertex.key)) break;
 
         maze.forEachNeighbor(vertex.key, [&vertex, &costMap, &frontier, &upTree, &maze, &end](Vector2 const &neighbor) {
-            if (
-                int const derivedCost{costMap.at(vertex.key) + /* cost to move adjacent */1},
-                decltype(costMap.end()) const costIter(costMap.find(neighbor));
-                costIter == costMap.end() or derivedCost < costIter->second
-            ) {
+            int const derivedCost{costMap.at(vertex.key) + /* cost to move adjacent */1};
+            auto const costIter(costMap.find(neighbor));
+
+            if (costIter == costMap.end() or derivedCost < costIter->second) {
                 costIter->second = derivedCost;
                 int priority{derivedCost + /* heuristic */maze.length(neighbor - end)};
-                frontier.emplace(vertex.key, priority);
+                frontier.push({vertex.key, priority});
                 upTree[neighbor] = vertex.key;
             }
         });
