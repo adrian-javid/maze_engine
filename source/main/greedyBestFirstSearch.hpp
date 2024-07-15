ed30f76ea571fb39afad66b51fccf2dede0cfb54
Adrian <73155005+adrian-javid@users.noreply.github.com>
2024-07-14 19:47:41 -0500

    add greedy best first search iterator and comparator

--- source/main/greedyBestFirstSearch.hpp
+++ source/main/greedyBestFirstSearch.hpp
@@ -11,6 +11,26 @@ namespace Project {
         Vector2 const &start, Vector2 const &end,
         std::function<bool(Vector2 const &)> const processKey=nullptr
     );
+
+    struct GreedyBestFirstSearchHeuristicComparator {
+        Maze const *maze; Vector2 end;
+        explicit GreedyBestFirstSearchHeuristicComparator(Maze const &maze, Vector2 end): maze(&maze), end(std::move(end)) {}
+        [[nodiscard]] bool operator()(Vector2 const &leftKey, Vector2 const &rightKey) const {
+            return maze->length(leftKey - end) > maze->length(rightKey - end);
+        };
+    };
+
+    using GreedyBestFirstSearchPriorityQueue = std::priority_queue<Vector2, std::vector<Vector2>, GreedyBestFirstSearchHeuristicComparator>;
+
+    struct GreedyBestFirstSearchIterator : AbstractSearchIterator<GreedyBestFirstSearchPriorityQueue> {
+        explicit GreedyBestFirstSearchIterator(Maze const &maze, Vector2 start, Vector2 end):
+            AbstractSearchIterator<GreedyBestFirstSearchPriorityQueue>(
+                maze,
+                std::move(start),
+                GreedyBestFirstSearchPriorityQueue(GreedyBestFirstSearchHeuristicComparator(maze, std::move(end)))
+            )
+        {}
+    };
 }
 
 #endif
