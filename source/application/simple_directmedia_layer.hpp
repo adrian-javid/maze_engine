aede93ba5c830b31778227f8c219be562f9be182
Adrian <73155005+adrian-javid@users.noreply.github.com>
2024-08-19 20:47:32 -0500

    disable all warnings for `SDL2/SDL.h` when building for Windows

--- source/application/simple_directmedia_layer.hpp
+++ source/application/simple_directmedia_layer.hpp
@@ -2,9 +2,14 @@
 #define Application_SimpleDirectmediaLayer_hpp
 
 #ifdef _WIN64
+#pragma warning(push, 0)
 #include "SDL2/SDL.h"
+#pragma warning(pop)
 #else
 #pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wall"
+#pragma GCC diagnostic ignored "-Wextra"
+#pragma GCC diagnostic ignored "-Wpedantic"
 #pragma GCC diagnostic ignored "-Wold-style-cast"
 #include "SDL.h"
 #pragma GCC diagnostic pop
