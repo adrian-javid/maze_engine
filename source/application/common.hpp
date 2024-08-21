1fcfebde46a58d8a15df396873a00c6b468ddf14
Adrian <73155005+adrian-javid@users.noreply.github.com>
2024-08-21 02:28:41 -0500

    remove unneeded includes

--- source/application/common.hpp
+++ source/application/common.hpp
@@ -2,14 +2,6 @@
 #define Application_App_hpp
 
 #include "simple_directmedia_layer.hpp"
-#if false
-#include "maze_engine/maze.hpp"
-#include "maze_engine/maze/square.hpp"
-#include "maze_engine/maze/hexagon.hpp"
-#include "maze_engine/vector2.hpp"
-#include <thread>
-#include <mutex>
-#endif
 
 // Application stuff.
 namespace App {
