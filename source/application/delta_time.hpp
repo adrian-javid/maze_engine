46d8fae7f29be6d30524b5cd8429265731e106c9
Adrian <73155005+adrian-javid@users.noreply.github.com>
2024-08-23 02:51:50 -0500

    type alias `UnsignedMilliseconds`

--- source/application/delta_time.hpp
+++ source/application/delta_time.hpp
@@ -4,7 +4,13 @@
 #include "simple_directmedia_layer.hpp"
 
 namespace App {
-	[[nodiscard]] Uint64 getDeltaTime();
+
+	using UnsignedMilliseconds = decltype(SDL_GetTicks64());
+	static_assert(std::is_integral_v<UnsignedMilliseconds>);
+	static_assert(std::is_unsigned_v<UnsignedMilliseconds>);
+
+	// Get change in time since last main loop iteration in milliseconds.
+	[[nodiscard]] UnsignedMilliseconds getDeltaTime();
 }
 
 #endif
