d8e68554ffe62f8b3892ee475c321d15a10279cf
Adrian <73155005+adrian-javid@users.noreply.github.com>
2024-08-30 07:22:45 -0500

    Change window to width & height 450. Add JavaScript function for changing window size.

--- source/application/window.cpp
+++ source/application/window.cpp
@@ -4,13 +4,17 @@
 #include <emscripten/bind.h>
 EMSCRIPTEN_BINDINGS(Window) {
 	emscripten::function("Window_setFullscreen", &App::Window::setFullscreen);
+	emscripten::function("Window_setSize", +[](int const width, int const height) {
+		assert(App::Window::window != nullptr);
+		SDL_SetWindowSize(App::Window::window, width, height);
+	});
 }
 #endif
 
 namespace App::Window {
 	SDL_Window *window{nullptr};
 	SDL_Renderer *renderer{nullptr};
-	int windowWidth{420};
-	int windowHeight{420};
+	int windowWidth{450};
+	int windowHeight{450};
 }
 
