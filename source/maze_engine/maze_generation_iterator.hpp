2421f4ef79f12f4650a7ea7261f7ce527dde8a12
Adrian <73155005+adrian-javid@users.noreply.github.com>
2024-10-11 02:57:05 -0500

    revise `Wall` to be used as the keys of a hash set

--- source/maze_engine/maze_generation_iterator.hpp
+++ source/maze_engine/maze_generation_iterator.hpp
@@ -8,12 +8,33 @@
 #include <algorithm>
 #include <random>
 #include <cassert>
+#include "maze_engine/auxiliary.hpp"
 
 namespace MazeEngine { class MazeGenerationIterator; }
 
 class MazeEngine::MazeGenerationIterator final {
 	public:
-		struct Wall { Vector2 tileKey; Maze::Direction type; };
+		struct [[nodiscard]] Wall {
+			Vector2 tileKey;
+			Maze::Direction type;
+			struct [[nodiscard]] Hash {[[nodiscard]] std::size_t operator()(Wall const &wall) const noexcept {
+				return Aux::combineHashValues(
+					Vector2::Hash{}(wall.tileKey),
+					std::hash<Aux::Enum::Int<Maze::Direction>>{}(Aux::Enum::asInt(wall.type))
+				);
+			}};
+
+			/// @brief An unordered set of `Vector2`.
+			using HashSet = std::unordered_set<Wall, Hash>;
+
+			[[nodiscard]]
+			constexpr bool operator==(Wall const &other) const {
+				return this->tileKey == other.tileKey and this->type == other.type;
+			}
+
+			[[nodiscard]]
+			constexpr bool operator!=(Wall const &other) const { return not(*this == other); }
+		};
 
 		[[nodiscard]] explicit MazeGenerationIterator(Maze &paramMaze, unsigned int const seed, bool const wrap=true);
 
