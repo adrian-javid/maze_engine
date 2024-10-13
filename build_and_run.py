cae71bbcca25fc79524248a12dd416ef407e518a
Adrian <73155005+adrian-javid@users.noreply.github.com>
2024-10-13 01:33:53 -0500

    have a parameter for the helper script forward arguments to built program

--- build_and_run.py
+++ build_and_run.py
@@ -16,12 +16,19 @@ def _main(argList: list[str]) -> None|str:
 	# Get an iterator to the arguments, and skip the first item.
 	argIter = iter(argList); next(argIter)
 
+	programArgList: list[str] = []
+
 	for arg in argIter:
 		match arg:
 			case "--release" | "--debug":
 				if buildType is not None:
 					return F"Can't set build type to \"{arg}\" because it was already set to \"{buildType}\"."
 				buildType = arg
+			case arg if arg.startswith("--arg"):
+				delimIndex: int = arg.find("=")
+				if delimIndex < 0:
+					return F"Received flag `--arg`, but there is no `=`."
+				programArgList += arg[delimIndex+1:].split()
 			case _:
 				return F"Received unrecognized flag \"{arg}\"."
 
@@ -36,7 +43,8 @@ def _main(argList: list[str]) -> None|str:
 	if (exitCode := subprocess.run(["scons", executablePath]).returncode) != os.EX_OK:
 		return F"Failed to build executable `{executablePath}`; exit code was `{exitCode}`."
 
-	if (exitCode := subprocess.run([executablePath]).returncode) != os.EX_OK:
+	print(F"Run \"{buildType.removeprefix("--")}\" build with arguments \"{programArgList}\":")
+	if (exitCode := subprocess.run([executablePath, *programArgList]).returncode) != os.EX_OK:
 		return F"Executable `{executablePath}` returned with exit code `{exitCode}`."
 
 	return None
