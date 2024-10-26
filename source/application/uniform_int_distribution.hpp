b05c22ecb25d2e968bf36069f019e1c54bbe57e0
Adrian <73155005+adrian-javid@users.noreply.github.com>
2024-10-26 00:48:49 -0500

    add uniform `int` distribution wrapper

---
+++ source/application/uniform_int_distribution.hpp
@@ -0,0 +1,31 @@
+#ifndef Application_UniformIntDistribution_hpp
+#define Application_UniformIntDistribution_hpp
+
+#include <random>
+
+namespace App { template <typename> class UniformIntDistribution; }
+
+template <typename IntT>
+class App::UniformIntDistribution {
+	public:
+		[[nodiscard]] inline IntT getInt() {
+			return uniformIntDistribution(randomNumberGenerator);
+		}
+
+		struct Range { IntT lowerBound{}, upperBound{}; };
+
+		[[nodiscard]] explicit
+		UniformIntDistribution() = delete;
+
+		[[nodiscard]] explicit
+		UniformIntDistribution(unsigned int const seed, Range const range):
+			randomNumberGenerator(seed),
+			uniformIntDistribution(range.lowerBound, range.upperBound)
+		{}
+
+	private:
+		std::mt19937 randomNumberGenerator = {};
+		std::uniform_int_distribution<IntT> uniformIntDistribution;
+};
+
+#endif
