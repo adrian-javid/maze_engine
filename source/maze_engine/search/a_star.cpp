cb0bda680c4458de505d69bfc8c703482fcfcdaf
Adrian <73155005+adrian-javid@users.noreply.github.com>
2024-10-29 22:46:52 -0500

    rename a variable in deprecated A* function to `tileKey`

--- source/maze_engine/search/a_star.cpp
+++ source/maze_engine/search/a_star.cpp
@@ -17,19 +17,19 @@ auto MazeEngine::aStarSearch(
 	Vector2::HashMap<int> costMap{{start, 0}};
 
 	while (not frontier.empty()) {
-		MetaVertex const vertex(frontier.top());
+		MetaVertex const tileKey(frontier.top());
 		frontier.pop();
 
-		if (processKey != nullptr and processKey(vertex.key)) break;
+		if (processKey != nullptr and processKey(tileKey.vector)) break;
 
-		maze.forEachNeighbor(vertex.key, [&vertex, &costMap, &frontier, &upTree, &maze, &end](Vector2 const &neighbor) {
-			int const derivedCost{costMap.at(vertex.key) + /* cost to move adjacent */1};
+		maze.forEachNeighbor(tileKey.vector, [&tileKey, &costMap, &frontier, &upTree, &maze, &end](Vector2 const &neighbor) {
+			int const derivedCost{costMap.at(tileKey.vector) + /* cost to move adjacent */1};
 
 			if (costMap.find(neighbor) == costMap.end() or derivedCost < costMap.at(neighbor)) {
 				costMap.insert({neighbor, derivedCost});
 				int priority{derivedCost + /* heuristic */maze.length(neighbor - end)};
 				frontier.push({neighbor, priority});
-				upTree.insert({neighbor, vertex.key});
+				upTree.insert({neighbor, tileKey.vector});
 			}
 		});
 	}
