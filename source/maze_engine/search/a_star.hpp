cf4b8e792f61051e914a6fe4bc64aa44fef1f416
Adrian <73155005+adrian-javid@users.noreply.github.com>
2024-10-29 22:47:58 -0500

    implement A* Search Iterator

--- source/maze_engine/search/a_star.hpp
+++ source/maze_engine/search/a_star.hpp
@@ -12,6 +12,64 @@ namespace MazeEngine {
 		Vector2 const &start, Vector2 const &end,
 		std::function<bool(Vector2 const &)> const processKey=nullptr
 	);
+
+	class AStarSearchIterator : public MazeSearchIterator {
+		public:
+			struct MetaVertex {
+				Vector2 vector{};
+				// Smaller priority is better.
+				int priority{};
+				inline bool operator>(MetaVertex const &vertex) const {
+					return priority > vertex.priority;
+				}
+			};
+
+			/*
+				The way this constructor is implemented,
+				the first vertex will be processed twice.
+
+				That's okay for the purposes of this program,
+				even though that is not how I prefer for it to work.
+			*/
+			explicit AStarSearchIterator(Maze const &paramMaze, Vector2 const start, Vector2 const paramEnd):
+				maze{&paramMaze}, currentTileKey(start), endTileKey(paramEnd),
+				upTree{{start, start}}, costMap{{start, 0}}
+			{
+				frontier.push(MetaVertex{start, 0});
+			}
+
+			void advance() override {
+				assert(not frontier.empty());
+				if (frontier.empty()) return;
+
+				MetaVertex const tileKey(frontier.top());
+				frontier.pop();
+
+				maze->forEachNeighbor(tileKey.vector, [this, tileKey](Vector2 const &neighbor) {
+					int const derivedCost{costMap.at(currentTileKey) + /* cost to move adjacent */1};
+
+					if (costMap.find(neighbor) == costMap.end() or derivedCost < costMap.at(neighbor)) {
+						costMap.insert({neighbor, derivedCost});
+						int const priority{derivedCost + /* heuristic */maze->length(neighbor - endTileKey)};
+						frontier.push({neighbor, priority});
+						upTree.insert({neighbor, tileKey.vector});
+					}
+				});
+			}
+
+			[[nodiscard]] Vector2 const &getVector() const override { return currentTileKey; }
+			[[nodiscard]] virtual bool isDone() const override { return frontier.empty(); }
+			[[nodiscard]] virtual Vector2::HashMap<Vector2> const &getHistory() const override { return upTree; }
+
+		private:
+			Maze const *maze{};
+			Vector2 currentTileKey{}, endTileKey{};
+			std::priority_queue<MetaVertex, std::vector<MetaVertex>, std::greater<MetaVertex>> frontier;
+			// (History.)
+			Vector2::HashMap<Vector2> upTree;
+			Vector2::HashMap<int> costMap;
+	};
+
 }
 
 #endif
