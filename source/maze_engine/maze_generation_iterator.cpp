0a8323746c273b25d4a369faadaa7f84c5c78b2e
Adrian <73155005+adrian-javid@users.noreply.github.com>
2024-11-02 22:55:36 -0500

    add comments

--- source/maze_engine/maze_generation_iterator.cpp
+++ source/maze_engine/maze_generation_iterator.cpp
@@ -27,10 +27,33 @@ MazeEngine::MazeGenerationIterator::MazeGenerationIterator(
 	App::println("wall count: ", wallList.size());
 }
 
+/*
+	In this function, the maze generation iterator must be advanced one.
+
+	This means that the iterator must process one more wall,
+	provided there is at least one wall leftover.
+*/
 auto MazeEngine::MazeGenerationIterator::advance() -> Result {
+	/*
+		It is likely a logic error to call this function when all of
+		the walls to process have been exhausted. In debug builds,
+		this assertion catches that case.
+	*/
 	assert(not isDone());
+
+	/*
+		We pay a little runtime cost to ensure that this function
+		doesn't invoke undefined behavior if the iterator is done.
+
+		If the iterator is done, there are no walls to process.
+	*/
 	if (isDone()) return Result::none;
 
+	/*
+		We are using the wall iterator like a stack data structure here.
+		By getting the wall and then incrementing the wall iterator,
+		we are effectively popping an element from the figurative stack.
+	*/
 	Wall const &wall{*wallIterator};
 	++wallIterator;
 
