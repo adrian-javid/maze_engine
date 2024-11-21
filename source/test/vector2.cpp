6a5b47e013ba8d60904375dc8d968a84e3b5b10d
Adrian <73155005+adrian-javid@users.noreply.github.com>
2024-11-21 16:53:42 -0600

    make vector tests compile-time

--- source/test/vector2.cpp
+++ source/test/vector2.cpp
@@ -1,26 +1,28 @@
-#include "catch2/catch_amalgamated.hpp"
 #include "maze_engine/vector2.hpp"
 #include "maze_engine/maze/square.hpp"
 
-using namespace MazeEngine;
 
-TEST_CASE("Neighbors", "[Vector2]") {
-	static constexpr int rowCount{20};
-	static constexpr int columnCount{20};
+namespace App::CompileTimeTests {
 
-	Vector2 const vector(1, 1);
-	CHECK(vector == Vector2(1, 1));
+	using namespace MazeEngine;
 
-	Vector2 const northNeighbor((vector + SquareMaze::northOffset).wrap(rowCount, columnCount));
-	CHECK(northNeighbor == Vector2(0, 1));
+	namespace Neighbors {
+		static constexpr int rowCount{20};
+		static constexpr int columnCount{20};
 
-	Vector2 const southNeighbor((vector + SquareMaze::southOffset).wrap(rowCount, columnCount));
-	CHECK(southNeighbor == Vector2(2, 1));
+		static constexpr Vector2 vector(1, 1);
+		static_assert(vector == Vector2(1, 1));
 
-	Vector2 const eastNeighbor((vector + SquareMaze::eastOffset).wrap(rowCount, columnCount));
-	CHECK(eastNeighbor == Vector2(1, 2));
+		static constexpr Vector2 northNeighbor((vector + SquareMaze::northOffset).wrap(rowCount, columnCount));
+		static_assert(northNeighbor == Vector2(0, 1));
 
-	Vector2 const westNeighbor((vector + SquareMaze::westOffset).wrap(rowCount, columnCount));
-	CHECK(westNeighbor == Vector2(1, 0));
+		static constexpr Vector2 southNeighbor((vector + SquareMaze::southOffset).wrap(rowCount, columnCount));
+		static_assert(southNeighbor == Vector2(2, 1));
 
+		static constexpr Vector2 eastNeighbor((vector + SquareMaze::eastOffset).wrap(rowCount, columnCount));
+		static_assert(eastNeighbor == Vector2(1, 2));
+
+		static constexpr Vector2 westNeighbor((vector + SquareMaze::westOffset).wrap(rowCount, columnCount));
+		static_assert(westNeighbor == Vector2(1, 0));
+	}
 }
